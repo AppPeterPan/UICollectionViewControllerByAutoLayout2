ec53d89a563adbd47c8a44c0bd4b55ef1020f8da
peterpan <apppeterpan@gmail.com>
2022-04-08 16:44:31 +0800

    Initial Commit

---
+++ Demo/PrinceCollectionViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  PrinceCollectionViewCell.swift
+//  Demo
+//
+//  Created by Peter Pan on 2022/4/8.
+//
+
+import UIKit
+
+class PrinceCollectionViewCell: UICollectionViewCell {
+    
+    static let reuseIdentifier = "\(PrinceCollectionViewCell.self)"
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageWidthConstraint: NSLayoutConstraint!
+    static let width = floor((UIScreen.main.bounds.width - 4) / 2)
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        imageWidthConstraint.constant = Self.width
+    }
+
+}
---
+++ Demo/PrinceCollectionViewController.swift
@@ -0,0 +1,41 @@
+//
+//  PrinceCollectionViewController.swift
+//  Demo
+//
+//  Created by Peter Pan on 2022/4/8.
+//
+
+import UIKit
+
+
+class PrinceCollectionViewController: UICollectionViewController {
+    let princes = (0...20).map { Prince(name: "小王子\($0)號", image: "pic\($0)") }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    // MARK: UICollectionViewDataSource
+    
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return princes.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: PrinceCollectionViewCell.reuseIdentifier, for: indexPath) as! PrinceCollectionViewCell
+        let prince = princes[indexPath.item]
+        cell.imageView.image = UIImage(named: prince.image)
+        cell.nameLabel.text = prince.name
+        return cell
+    }
+    
+    
+}
